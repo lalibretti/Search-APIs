09ab7981f352a96015f2640dee47d1f4869a9445
Lauren Libretti <lalibretti@yahoo.com>
2021-04-21 22:29:00 -0400

    working fetch

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
-//http://shibe.online/api/shibes?count=[1-100]&urls=[true/false]&httpsUrls=[true/false]
 const apiKey ='mlye8iDHtLK95jzzL2sydUiQFmWe5nEy';
-window.addEventListener('load', (event) => {
-    console.log('page has loaded');
-    //page reloads onclick
-    document.getElementById('search').addEventListener('click', () => {
-    console.log('button clicked!')
-    let searchTerm = document.getElementById('search-term').value;
-    console.log('search term is', searchTerm);
 
-        fetch('https://api.giphy.com/v1/gifs/search?api_key=mlye8iDHtLK95jzzL2sydUiQFmWe5nEy&q=${searchTerm}&limit=25&offset=0&rating=g&lang=en')
+    
+   window.addEventListener('load', (event) => {
+    console.log('page is loaded');
+    document.getElementById('search-button').addEventListener('click', () => {
+        console.log('button was clicked');
+        let searchTerm = document.getElementById('search-term').value;
+        console.log('search term is', searchTerm);
+        
+        fetch(`https://api.giphy.com/v1/gifs/search?api_key=mlye8iDHtLK95jzzL2sydUiQFmWe5nEy&q=${searchTerm}&limit=25&offset=0&rating=g&lang=en`)
         .then((res) => {
-            return res.json() 
-        }) .then ((json) => {
-            console.log(json['data']);
-            //console.log('api data', data)
-           // console.log(`<img src = ${data.message}/>`)
-            //document.getElementById('.result').innerHTML = `<img src =${ data.message} />`
-        });
-    })
-    })
+            return res.json()
+        }).then((json) => {
+            console.log(json);
+            let results = json['items'];
+            console.log('results array:', results);
 
-    //works at page load!! 
-//     const url = 'https://dog.ceo/api/breed/hound/images';
-//     document.getElementById('btn').addEventListener('click',
-//         fetch(url).then(function (res) {
-//             console.log(res);
-//             return res.json()
-//         })
-//             .then(function (data) {
-//                 console.log(data);
-//                 data.data.array.forEach(function (val) {
-//                     console.log(val);
-                    
-//                 });
-//             })
-// )})
+            document.getElementById('results').innerHTML = "";
+            for( let result of results) {
+                console.log(result.images.original)
+            }
+
+        })
+    });
+})
+//works at page load!! 
+// const url = 'https://api.giphy.com/v1/gifs/search?api_key=mlye8iDHtLK95jzzL2sydUiQFmWe5nEy&q=${searchTerm}&limit=25&offset=0&rating=g&lang=en';
+// document.getElementById('btn').addEventListener('click')
+//     fetch(url).then(function (res) {
+//         console.log(res);
+//         return res.json()
+//     })
+//         .then(function (data) {
+//             console.log(data);
+//             data.data.array.forEach(function (val) {
+//                 console.log(val);
+
+//             });
