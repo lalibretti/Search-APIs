b2ba94b272e99dfe6307713583a9a317129acbd8
Lauren Libretti <lalibretti@yahoo.com>
2021-04-21 20:12:04 -0400

    started api

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 //http://shibe.online/api/shibes?count=[1-100]&urls=[true/false]&httpsUrls=[true/false]
-
+const apiKey ='mlye8iDHtLK95jzzL2sydUiQFmWe5nEy';
 window.addEventListener('load', (event) => {
     console.log('page has loaded');
     //page reloads onclick
     document.getElementById('search').addEventListener('click', () => {
-    //console.log('button clicked!'); 
-        fetch('https://dog.ceo/api/breed/hound/images')
+    console.log('button clicked!')
+    let searchTerm = document.getElementById('search-term').value;
+    console.log('search term is', searchTerm);
+
+        fetch('https://api.giphy.com/v1/gifs/search?api_key=mlye8iDHtLK95jzzL2sydUiQFmWe5nEy&q=${searchTerm}&limit=25&offset=0&rating=g&lang=en')
         .then((res) => {
             return res.json() 
-        }) .then ((data) => {
-            console.log(data);
+        }) .then ((json) => {
+            console.log(json['data']);
             //console.log('api data', data)
            // console.log(`<img src = ${data.message}/>`)
             //document.getElementById('.result').innerHTML = `<img src =${ data.message} />`
